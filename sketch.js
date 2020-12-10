2a2b2bcffa3ff70bd05fdf2c2e4587f5d98dbbbd
Avasudevan270 <74630207+Avasudevan270@users.noreply.github.com>
2020-12-10 15:06:40 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var movingRect,fixedRect
+
+function setup(){
+  createCanvas(400,400)
+
+  movingRect = createSprite(200,100,50,80)
+  fixedRect = createSprite(200, 300, 80, 50)
+
+
+  fixedRect.shapeColor= "red"
+  movingRect.shapeColor= "red"
+
+  movingRect.velocityY= 2
+  fixedRect.velocityY=-2
+
+}
+function draw(){
+ background(0)
+
+
+
+
+if(movingRect.x-fixedRect.x <= (movingRect.width+fixedRect.width)/2 &&
+fixedRect.x-movingRect.x <= (movingRect.width+fixedRect.width)/2 ){
+movingRect.velocityX=movingRect.velocityX*-1
+fixedRect.velocityX=fixedRect.velocityX*-1
+}
+if(movingRect.y-fixedRect.y <= (movingRect.height+fixedRect.height)/2 &&
+fixedRect.y-movingRect.y <= (movingRect.height+fixedRect.height)/2){
+  movingRect.velocityY=movingRect.velocityY*-1
+fixedRect.velocityY=fixedRect.velocityY*-1
+}
+
+
+  drawSprites();
+}
